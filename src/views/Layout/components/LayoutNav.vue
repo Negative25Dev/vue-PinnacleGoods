2f7764535248c6f34ce9a3794d9db1678e4d7226
mm <1779419575@qq.com>
2025-11-28 15:06:37 +0800

    会员中心-个人信息数据渲染

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -27,6 +27,7 @@ const confirm = () => {
           <li><a href="javascript:;">我的订单</a></li>
           <li><a @click="$router.push('/member')">会员中心</a></li>
         </template>
+
         <template v-else>
           <li><a href="javascript:;" @click="$router.push('/login')">请先登录</a></li>
           <li><a href="javascript:;">帮助中心</a></li>
