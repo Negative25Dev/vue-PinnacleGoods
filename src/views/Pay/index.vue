10e061fb382e8ee7866a7d934bd33731889d95f6
mm <1779419575@qq.com>
2025-11-28 13:26:06 +0800

    倒计时业务实现

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -26,6 +26,7 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
 
 
 <template>
+
     <div class="xtx-pay-page">
         <div class="container">
             <!-- 付款信息 -->
@@ -33,7 +34,7 @@ const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redire
                 <span class="icon iconfont icon-queren2"></span>
                 <div class="tip">
                     <p>订单提交成功！请尽快完成支付。</p>
-                    <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+                    <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
                 </div>
                 <div class="amount">
                     <span>应付总额：</span>
